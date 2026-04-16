87d2800e6c8a22b215d7ebe73f592d784c4bf0cf
salatomato-0248 <rarintornpao@gmail.com>
2026-04-16 20:22:22 +0700

    แก้ err หน้า app\layout
    ......

--- AiHealthCoachApp/app/_layout.tsx
+++ AiHealthCoachApp/app/_layout.tsx
@@ -8,17 +8,17 @@ import { WorkoutPlanProvider } from "@/context/WorkoutPlanContext";
 export default function RootLayout() {
   return (
     <ProfileProvider>
-    <WorkoutPlanProvider>
-      <Stack screenOptions={{ headerShown: false }}>
-        {/* หน้า index หลัก */}
-        <Stack.Screen name="index" />
-        <Stack.Screen name="(onboarding)/SetUpYourHealthProfile" />
-        
-        {/* บอก Stack ว่ามีกลุ่มหน้าในโฟลเดอร์ auth */}
-        <Stack.Screen name="(auth)/Register" />
-        <Stack.Screen name="(auth)/Login" />
-      </Stack>
-    </WorkoutPlanProvider>
+      <WorkoutPlanProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+          {/* หน้า index หลัก */}
+          <Stack.Screen name="index" />
+          <Stack.Screen name="(onboarding)/SetUpYourHealthProfile" />
+          
+          {/* บอก Stack ว่ามีกลุ่มหน้าในโฟลเดอร์ auth */}
+          <Stack.Screen name="(auth)/Register" />
+          <Stack.Screen name="(auth)/Login" />
+        </Stack>
+      </WorkoutPlanProvider>
     </ProfileProvider>
   );
 }
